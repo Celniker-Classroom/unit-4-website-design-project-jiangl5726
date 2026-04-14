06912850d32c085aaab758beccf12e062bf90268
jiangl5726 <jiangl5726@my.sduhsd.net>
2026-04-14 01:01:10 +0000

    one java

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// add javascript here
+var prevScrollpos = window.pageYOffset;
+var navbar = document.querySelector(".navbar"); // select it by the class
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    // makes it so scrolling up will show navbar
+    navbar.style.top = "0";
+  } else {
+    // makes it so scrolling down will hide navbar
+    navbar.style.top = "-100px"; // fully hides navbar
+  }
+  prevScrollpos = currentScrollPos;
+};
